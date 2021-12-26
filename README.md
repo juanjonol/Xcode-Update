6b41f72a3427b58fffa5b556f57e560591081767
Juanjo López <juanjonol@gmail.com>
2021-12-26 12:57:29 +0100

    Added initial (nonfunctional) implementation
    Instead of my initial idea of having to indicate how many Betas and Release versions to preserve, now this simply automatically installs the latest version (whatever it’s a Beta or Release version) and deletes the oldest one (the oldest Release if `/Applications/Xcode.app` was updated, the oldest Beta otherwise).
    
    This heavily simplifies the usage and implementation of this script.
    
    - Now the user can simply install as much Xcode versions as they want, with this script only deleting the oldest one.
    - With my previous idea, we would had to add a configuration file in the near future (to indicate how many versions to preserve, because it’d have been annoying having to specify it on each invocation).
    - A `non-interactive` method is also added to allow this to work completely unsupervised (but it’s not the default, to let the user check what this script is going to do).
    - This implementation is far for perfect (there’re a few redundant calls to `xcodes`) but performance is lest important than clarity.

--- README.md
+++ README.md
@@ -6,16 +6,34 @@ Simplify the update of Xcode versions.
 
 [`xcodes`](https://github.com/RobotsAndPencils/xcodes) makes managing Xcode's versions a lot easier, but the user must still choose which versions to install or delete.
 
-This script is a simple wrapper around `xcodes` to automate this as much as possible. 
+This script is a simple wrapper around `xcodes` to automate this as much as possible.
+
+This is an heavily-opinionated script that in general does the following changes:
+
+- The latest Xcode version is always installed, no matter if it's a Beta (Prerelease) or a Release version.
+- `/Applications/Xcode-beta.app` is set as a symbolic link pointing to the latest Beta.
+- `/Applications/Xcode.app` is set as a symbolic link pointing to the latest Release, but only if `/Applications/Xcode-beta.app` isn't already pointing to it (there’s no point on having two links to the same version).
+   - This will usually happen with Release Candidates, which are a Prerelease versions that should end up as Release versions.   
+   - We can think of this as keeping the latest Release version as a Beta until it’s properly tested. so we keep the previous Release version until it's property tested.
+- After everything is updated, the oldest version is deleted.
+   - The oldest Release version is only deleted when `/Applications/Xcode.app` version is updated.
 
 ## Usage
 
 Simply call `xcode-update` to download a new Xcode version and delete the old one.
 
-By default, only the latest release and beta version is preserved, but a different number can be set with the `--release` and `--betas` parameters. For example, `xcode-update --beta 2` will preserve 1 release version and 2 beta versions.
+## FAQ
+
+### How do I manage multiple Xcode versions with this script?
+
+This script only installs the newest Xcode version and deletes the oldest one. Any other Xcode version will be let untouched.
+
+### How do I keep this script from deleting an Xcode version?
+
+This script uses `xcodes list` to search for Xcode versions, so anything that hides the Xcode version from `xcodes` will work. For example, the Xcode version could be placed on an "Archived" subfolder.
 
 ## Support
 
-If you have a problem, create an [issue][1]. Pull request are welcome.
+If you have a problem, create an [issue][1]. Pull request are welcome, but keep in mind that this script is heavily tailored to how I want ot to work, so I don't intent to change its behaviour too much (unless I'm convinced otherwise).
 
 [1]:	https://github.com/juanjonol/Xcode-Updater/issues
--- xcode-update.py
+++ xcode-update.py
@@ -9,7 +9,17 @@ import sys
 from shutil import which
 import argparse
 import subprocess
+from pathlib import Path
 
+
+# Symlink to the latest Xcode release version.
+XCODE_RELEASE = Path(f'/Applications/Xcode.app')
+# Symlink to the latest Xcode prerelease version.
+XCODE_BETA = Path(f'/Applications/Xcode-beta.app')
+# String that Xcodes adds to a version to indicate it's already installed. Note the space at the beginning.
+XCODES_INSTALLED_MAGIC_STRING = ' (Installed)'
+# String that Xcodes adds to a version to indicate it's a Beta version.
+XCODES_BETA_MAGIC_STRING = 'Beta'
 
 def parse_args():
 	"""Parse the Command Line arguments and generate the Command Line help."""
@@ -17,8 +27,7 @@ def parse_args():
 	root_parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)  # Uses file's default docstring
 	# Arguments
 	root_parser.add_argument('-v', '--version', action='version', version='1.0.0')
-	root_parser.add_argument('-r', '--release', type=int, default=1, help='The number of Xcode release versions to preserve. By default, only 1 release version is preserved')
-	root_parser.add_argument('-b', '--betas', type=int, default=1, help='The number of Xcode beta versions to preserve. By default, only 1 beta version is preserved')
+	root_parser.add_argument('-n', '--non-interactive', action='store_false', dest='interactive', help='Installs and deletes Xcode versions without asking for permission first')
 	return root_parser.parse_args()
 
 
@@ -31,28 +40,110 @@ def main():
 		print("WARNING: aria2 is not installed. This makes downloading Xcode versions significantly slower. You can install it with `brew install aria2`.")
 
 	args = parse_args()
-	if args.release < 1 or args.betas < 1:
-		raise AssertionError("Invalid value of Xcode versions to preserve (it must be more than 1)")
-	install_latest_xcode_versions()
-	delete_unnecessary_xcode_versions(args.release, args.betas)
+	if args.interactive:
+		install_latest_xcode(dry_run=True)
+		update_symlinks(dry_run=True)
+		delete_xcode(dry_run=True)
+		ask_for_confirmation("Continue updating? (Y/n): ")
+	install_latest_xcode(dry_run=False)
+	update_symlinks(dry_run=False)
+	delete_xcode(dry_run=False)
 	
 	
-def install_latest_xcode_versions():
-	"""Install the latest Xcode versions, if they aren't already installed."""
+def install_latest_xcode(dry_run: bool):
+	"""Install the latest Xcode version, if it isn't already installed."""
 	
-	subprocess.run(['xcodes', 'install', '--latest'], check=True)
-	subprocess.run(['xcodes', 'install', '--latest-prerelease'], check=True)
+	latest_version = latest_xcode_version()
+	if XCODES_INSTALLED_MAGIC_STRING in latest_version:
+		latest_version = latest_version.replace(XCODES_INSTALLED_MAGIC_STRING, '')
+		print(f'{latest_version} is already installed. Nothing to do.')
+		exit()
+	print(f'- Xcode version {latest_version} will be installed.')
 	
+	if not dry_run:
+		print("Installation of Xcode versions is not implemented yet")
+		#subprocess.run(['xcodes', 'install', '--latest-prerelease'], check=True)
 
-def delete_unnecessary_xcode_versions(number_release: int, number_betas: int):
-	"""Deletes old Xcode versions."""
 
+def update_symlinks(dry_run: bool):
+	"""Updates the symlinks to the latest Xcode version."""
+
+	latest_version = latest_xcode_version()
+	current_beta = None
+	if not XCODE_BETA.exists():
+		print(f'- {XCODE_BETA} will be created pointing to Xcode {latest_version}.')
+	else:
+		current_beta = XCODE_BETA.resolve()
+		print(f'- {XCODE_BETA} will stop linking to {current_beta} and start pointing to Xcode {latest_version}.')
+	# Xcode release versions are only updated when the current beta (that's about to be replaced) points to a release version
+	if is_release_version(current_beta):
+		if not XCODE_RELEASE.exists():
+			print(f'- {XCODE_RELEASE} will be created pointing to {current_beta}.')
+		else:
+			current_release = XCODE_RELEASE.resolve()
+			print(f'- {XCODE_RELEASE} will stop linking to {current_release} and start pointing to {current_beta}.')
+
+	if not dry_run:
+		print("The update of the symlinks is not implemented yet")
+
+
+def delete_xcode(dry_run: bool):
+	"""Deletes the oldest Xcode version."""
+
+	# Xcode release versions are only updated when the current beta (that's about to be replaced) points to a release version
+	current_beta = XCODE_BETA.resolve()
+	should_delete_release_version = is_release_version(current_beta)
+	xcode_version_to_delete = oldest_xcode_version(include_releases=should_delete_release_version)
+	if xcode_version_to_delete:
+		print(f'- {xcode_version_to_delete} will be deleted.')
+	
+	if not dry_run:
+		print(f'Deleting {str(xcode_version_to_delete)}...')
+		print("Deletion of Xcode versions is not implemented yet")
+		
+		
+def ask_for_confirmation(prompt: str):
+	"""Shows the prompt and waits for the user to answer. If the user answer "No", the program is halted."""
+	
+	answer = None
+	while answer not in ('y', 'n', ''): # '' = "Enter" key, by default we let the script continue
+		answer = input(prompt)
+		if answer: # If the answer is the enter key, the string is empty
+			answer = answer[0].lower()
+	if answer == 'n':
+		exit()
+	
+	
+def latest_xcode_version() -> str:
+	"""Returns the latest Xcode version available"""
+	
+	all_versions = subprocess.run(['xcodes', 'list'], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+	return all_versions.split("\n")[-2] # The last string (-1 on the array) is always empty
+	
+	
+def is_release_version(path: Path) -> bool:
+	"""Verifies if the supplied Xcode version is a Release version"""
+	
+	if path is None:
+		return False
 	installed_versions = subprocess.run(['xcodes', 'installed'], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-	print("Deletion of Xcode versions is not implemented yet")
-	# TODO: Distinguish between release and beta versions
-	# TODO: Actually delete Xcode versions
+	for version in installed_versions.split("\n"):
+		if str(path) in version:
+			return not XCODES_BETA_MAGIC_STRING in version
+	AssertionError(f"{path} doesn't seems to be a valid Xcode version")
+	
+	
+def oldest_xcode_version(include_releases: bool) -> Path:
+	"""Returns the path to the oldest Xcode version, to be able to delete it.
+	The parameter allows to only delete beta versions (to keep a release version even if it's older)."""
+	
+	installed_versions = subprocess.run(['xcodes', 'installed'], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+	for version in installed_versions.split("\n"):
+		if include_releases | (XCODES_BETA_MAGIC_STRING in version):
+			return version.split("\t")[1]
+	return None # There could be no previous Xcode versions
 
 
 if __name__ == '__main__':
-	sys.tracebacklimit = 0
+	sys.tracebacklimit = 0 # Disables Python's Traceback, to show clearer errors.
 	sys.exit(main())
